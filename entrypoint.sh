5e4ed777b0f0147ef89b479849faf0e166e7ea61
alex289 <mccr4fthd@gmail.com>
2024-02-01 19:39:47 +0100

    fix: Does this work

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-# Input variables
-SOURCE_REPO=$1
-DESTINATION_REGISTRY=$2
-DESTINATION_REPO=$3
-
-if [ -z "$DESTINATION_REPO" ]
-then
-  DESTINATION_REPO=$SOURCE_REPO
-fi
-
 # Log in to Docker Hub
 if [ -n "$SOURCE_REGISTRY_PASSWORD" ] && [ -n "$SOURCE_REGISTRY_USERNAME" ]
 then
@@ -21,12 +11,8 @@ fi
 # Pull the image from Docker Hub
 docker pull $SOURCE_REPO
 
-# Get the tag of the image
-IMAGE_TAG=$(docker inspect --format='{{index .RepoTags 0}}' $SOURCE_REPO)
-echo "Image tag: $IMAGE_TAG"
-
 # Tag the image for the destination registry
-DESTINATION_IMAGE="$DESTINATION_REGISTRY/$DESTINATION_REPO:$IMAGE_TAG"
+DESTINATION_IMAGE="$DESTINATION_REGISTRY/$DESTINATION_REPO"
 docker tag $SOURCE_REPO $DESTINATION_IMAGE
 
 # Log in to the destination registry
